3ac9ca54af5dc25abb67231f1205ceda7195b140
JessHuyser <u20523158@tuks.co.za>
2022-05-05 20:13:36 +0200

    update

--- src/app/User/read-user/read-user.component.ts
+++ src/app/User/read-user/read-user.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { VentrixDBServiceService } from 'src/app/services/ventrix-db-service.service';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { User } from 'src/app/shared/User';
+import { Router } from '@angular/router';
+//Make sure swal is imported
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-read-user',
@@ -7,9 +13,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReadUserComponent implements OnInit {
 
-  constructor() { }
+  users:any[] = [];
+  constructor(private ventrixdbservice:VentrixDBServiceService, private router: Router) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void 
+  {
+    this.ventrixdbservice.readUser()
+    .subscribe(response => {
+      this.users = response;
+      console.log(this.users)
+    })
   }
 
+  addUser()
+  {
+    this.router.navigate(['/create-user']);
+  }
+
+  editUser(selecteduser: User)
+  {
+      this.ventrixdbservice.setUser(selecteduser);
+      this.router.navigate(['/update-user']);
+  }
+
+  //Delete User Function 
+  deleteUser(selecteduser: User)
+  { 
+      //Sweet alerts are used as notifications
+      Swal.fire({
+        icon: 'warning',
+        title: 'Are you sure you want to delete this user?',
+        showDenyButton: true,
+        confirmButtonText: 'Yes',
+        denyButtonText: `No`,
+        confirmButtonColor: '#077bff',
+        allowOutsideClick: false,
+        allowEscapeKey: false
+      }).then((result) => {
+        if (result.isConfirmed) {
+          this.ventrixdbservice.deleteUser(selecteduser).subscribe();
+          this.router.navigate(['/read-user']).then(() => {
+          window.location.reload();
+          });
+        }
+      })  
+  }
 }
--- src/app/User/update-user/update-user.component.ts
+++ src/app/User/update-user/update-user.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { User } from 'src/app/shared/User';
+import { Router } from '@angular/router';
+import { VentrixDBServiceService } from 'src/app/services/ventrix-db-service.service';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-update-user',
@@ -7,9 +13,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateUserComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  userform : FormGroup;
+  user: User|undefined;
+  submitted = false;
+  constructor(fbuilder: FormBuilder, private router: Router,private ventrixdbservice:VentrixDBServiceService)
+  {
+      //Additional Validation can be added here
+      this.userform = fbuilder.group({
+      //User ID is not displayed but is neccessary for the API to update
+      userId: new FormControl ('',[Validators.required]),
+      userRoleId: new FormControl ('',[Validators.required]),
+    });
   }
 
+  //Populate Input values
+  ngOnInit(): void 
+  {
+    this.user = this.ventrixdbservice.getUser();
+    this.userform.patchValue({
+    userId: this.user?.UserId,
+    userRoleId: this.user?.UserRoleId,
+    })
+
+    this.ventrixdbservice.clearClient();
+  }
+
+  updateUser()
+  {
+    this.ventrixdbservice.updateUser(this.userform.value).subscribe();
+    //redirects back to data table and refreshes page
+    //Sweet alerts are used as notifications
+    Swal.fire({
+      icon: 'success',
+      title: 'User Updated Successfully',
+      confirmButtonText: 'OK',
+      confirmButtonColor: '#077bff',
+      allowOutsideClick: false,
+      allowEscapeKey: false
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.router.navigate(['/read-user']).then(() => {
+          window.location.reload();
+        });
+      }
+    })  
+  }
+
+  //When Cancel button clicked returns to Read User screen
+  returnDataTable()
+  {
+    this.router.navigate(['/read-user']);
+  }
 }
